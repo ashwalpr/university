3d104326f78973f8355c4d164babdadae0c2a307
Ashwal <ashwal.p.r1995@gmail.com>
2018-08-29 15:02:20 -0700

    changed

--- js/script.js
+++ js/script.js
@@ -12,6 +12,75 @@ window.addEventListener('scroll', function() {
   }
 });
 
+/**************************************
+                  toggle
+**************************************/
+document.getElementById('toggleMobileNav').addEventListener('click',function() {
+  if(document.getElementById('mainnav').classList.contains('open')){
+    document.getElementById('mainnav').classList.remove('open');
+  } else {
+    document.getElementById('mainnav').classList.add('open');
+  }
+})
+
+/*****************************
+          numbers
+*****************************/
+var numbers = document.getElementsByClassName('number');
+ console.log(numbers)
+  var numberArray = [];
+  for(var i = 0; i < numbers.length; i++) {
+    numberArray.push(parseInt(numbers[i].innerHTML));
+  }
+  var time = 4;
+  var startNo = 0;
+  for(var i = 0; i < numbers.length; i++) {
+    numbers[i].innerHTML = 0;
+  }
+  var incArray = [];
+  for(var i = 0; i < numbers.length; i++) {
+    incArray.push(parseInt(Math.ceil(numberArray[i]/250)));
+  }
+  var startArray = [];
+  for(var i = 0; i < numbers.length; i++) {
+    startArray.push(0);
+  }
+  var statusArray = [];
+  for(var i = 0; i < numbers.length; i++) {
+    statusArray.push(false);
+  }
+  console.log(numberArray, incArray, startArray)
+
+  var count = 0;
+  var updateNumbers = setInterval(function() {
+    for(var i = 0; i < numbers.length; i++) {
+      if(statusArray[i] == false) {
+        startArray[i] += incArray[i];
+      }
+      numbers[i].innerHTML = startArray[i]
+
+      if(startArray[i] >= numberArray[i]) {
+        statusArray[i] = true;
+        console.log(statusArray)
+      }
+    }
+    for(var i = 0; i < statusArray.length; i++) {
+      if(statusArray[i] == true) {
+        ++count;
+      }
+    }
+    if(count == 4) {
+      clearInterval(updateNumbers)
+    } else {
+      count = 0;
+    }
+
+  }, time);
+
+
+
+
+
 /********************************
     timer logic
 *********************************/
